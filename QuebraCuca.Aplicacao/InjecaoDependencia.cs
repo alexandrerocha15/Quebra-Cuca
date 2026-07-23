36feb5626b350a3284ea10373430f262f68209f3
Alexandre Rocha <juny14_15@hotmail.com>
2026-07-23 13:41:04 -0300

    Adicionado Modulos e Dependências

---
+++ QuebraCuca.Aplicacao/InjecaoDependencia.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuebraCuca.Aplicacao;
+
+public static class InjecaoDependencia
+{
+    public static void AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+
+    }
+}
--- QuebraCuca.WebApp/Compartilhado/Mapping/AutoMapperOptions.cs
+++ QuebraCuca.WebApp/Compartilhado/Mapping/AutoMapperOptions.cs
@@ -2,7 +2,7 @@ namespace QuebraCuca.WebApp.Compartilhado.Mapping;
 
 public sealed class AutoMapperOptions
 {
-    public const string SectionName = "AutoMapper";
+    public const string SectionName = "Apresentacao:AutoMapper";
 
     public string? LicenseKey { get; init; }
 }
